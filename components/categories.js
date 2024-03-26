d728a9d9b3539a634a36daf4e00d6d09f4a7e0f6
Jennifer Yiu <jen.yiu.90@gmail.com>
2024-03-25 17:55:00 -0700

    add screens and assets

---
+++ components/categories.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Categories() {
+  return (
+    <View className="mt-4">
+      <Text>Categories</Text>
+    </View>
+  )
+}
